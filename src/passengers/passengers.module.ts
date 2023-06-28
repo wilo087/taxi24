3fc530757aa07be914e71e642c01c6493fb10add
Wilowayne De La Cruz G <wilo0087@gmail.com>
2023-06-27 21:59:10 -0400

    Injecting prisma db instance to trip and passenger modules

--- src/passengers/passengers.module.ts
+++ src/passengers/passengers.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common'
 import { PassengersController } from './controllers/passengers.controller'
 import { PassengersService } from './services/passengers.service'
+import { PrismaModule } from 'src/prisma/prisma.module'
 
 @Module({
   controllers: [PassengersController],
-  providers: [PassengersService]
+  providers: [PassengersService],
+  exports: [PassengersService],
+  imports: [PrismaModule],
 })
 
 export class PassengersModule { }
--- src/trips/trips.module.ts
+++ src/trips/trips.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common'
 import { TripsController } from './controllers/trips.controller'
 import { TripsService } from './services/trips.service'
+import { PrismaModule } from 'src/prisma/prisma.module'
 
 @Module({
   controllers: [TripsController],
-  providers: [TripsService]
+  providers: [TripsService],
+  exports: [TripsService],
+  imports: [PrismaModule],
 })
 
 export class TripsModule { }
