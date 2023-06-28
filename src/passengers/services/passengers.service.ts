51ee70da6b16acd36069d83cced164f6e8a83ac7
Wilowayne De La Cruz G <wilo0087@gmail.com>
2023-06-27 21:47:06 -0400

    Adding services logic

--- src/passengers/services/passengers.service.ts
+++ src/passengers/services/passengers.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { Passenger } from '@prisma/client'
 
 @Injectable()
-export class PassengersService {}
+export class PassengersService {
+  constructor(private prisma: PrismaService) { }
+
+  async findAll(): Promise<Passenger[]> {
+    return this.prisma.passenger.findMany();
+  }
+
+  async findOne(id: number): Promise<Passenger | null> {
+    return this.prisma.passenger.findUnique({
+      where: { id }
+    })
+  }
+
+  // TODO: We need to calculate the distance between the passenger and the driver
+  async requestTrip(lat: number, lng: number): Promise<Passenger[] | unknown> {
+    const drivers = await this.prisma.driver.findMany({
+      where: {
+        AND: [{ currentLat: lat }, { currentLon: lng }]
+      }
+    })
+
+    return drivers
+  }
+}
--- src/trips/services/trips.service.ts
+++ src/trips/services/trips.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { Trip, TripsStatus } from '@prisma/client'
 
 @Injectable()
-export class TripsService {}
+export class TripsService {
+  constructor(private prisma: PrismaService) { }
+
+  async findAll(): Promise<Trip[]> {
+    return this.prisma.trip.findMany();
+  }
+
+  async findOne(id: number): Promise<Trip | null | unknown> {
+    return this.prisma.trip.findUnique({
+      where: { id }
+    })
+  }
+
+  async create(data: any): Promise<Trip> {
+    return this.prisma.trip.create({
+      data
+    })
+  }
+
+  async completeTrip(id: number): Promise<Trip> {
+    return this.prisma.trip.update({
+      where: { id },
+      data: { status: TripsStatus.COMPLETED }
+    })
+  }
+}
